b4879f1d218be61106aba240a0998f56c2f3033d
wongakim-99 <202021000@sangmyung.kr>
2024-05-09 20:54:40 +0900

    #6 - TOodoItemList 컴포넌트 생성
    등록된 Todo 아이템 보여줌
    Show_registered_items 브랜치 생성하고 이곳에서 작업

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import TodoItemInputField from "./TodoItemInputField";
+import TodoItemList from "./TodoItemList";
 
 const App = () => {
   return (
     <div className="App">
       <TodoItemInputField onSubmit={() => {}} />
+      <TodoItemList />
     </div>
   );
 };
---
+++ todolist/src/TodoItemList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoItemList = (props) => {
+  return <div>TodoItemList</div>;
+};
+
+export default TodoItemList;
