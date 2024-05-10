9b90a5ef62b7393c1a859c6f276d55b842638e8e
wongakim-99 <202021000@sangmyung.kr>
2024-05-10 17:09:11 +0900

    #14 - firebase 설치 및 초기 세팅 진행
    step by step
    
    1. npm install firebase
    
    2. Add Firebase SDK / Firebase SDK 추가하기
    Copy/paste displayed code into src/App.js
    
    3. npm install -g firebase-tools
    
    4. firebase login
    
    5. firebase init
    
    6. firebase init 후 아래와 같은 옵션 설정
    
    6-1>Select: “Hosting: Configure files for Firebase Hosting and (optionally)
    
    6-2>Use an existing project
    (내가 진행하려는 프로젝트)
    
    6-3>Hosting: Set up GitHub Action deploys
    
    7. Use following answers for the following questions: / 다음 질문들에 다음과 같이 답하기
    What do you want to use as your public directory?
    build
    
    Configure as a single-page app (rewrite all urls to /index.html)?
    y
    
    Set up automatic builds and deploys with GitHub?
    N
    
    File build/index.html already exists. Overwrite?
    N
    
    8. firebase deploy
    
    ※※※ 만약 firebase login 입력시 아래와 같은 로그인 오류가 생길수 있다. ※※※
    Error : An unexpected error has occurred
    
    위와 같은 오류 발생시 당황하지 말고 아래와 같은 단계를 따르기
    
    1. visual studio code 를 관리자 권한으로 실행
    2. Window 사용자일 경우 터미널 창을 Powershell 말고 Gitbash 사용
    3. Gitbash 터미널 창에서 firebase login --no-localhost
    입력
    4. 그러면 로그인 인증에 필요한 순서들이 적혀 있고 마지막에 인증 코드를 적으라고 할 것이다.
    5. 마지막 url복사해서 터미널 창에 붙혀 넣으면 끝
    
    login init으로 세팅 진행하고 파이어베이스 사이트에 본인이 진행하는 프로젝트에 가서 데이터베이스 생성하기(위치 적을땐 본인이 제일 가까운 지역 하는게 좋음, 저는 서울로 했습니다.)
    
    그러고 나서 데이터 베이스는 테스트 모드로 생성한다.

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -3,6 +3,28 @@ import TodoItemInputField from "./TodoItemInputField";
 import TodoItemList from "./TodoItemList";
 import { useState } from "react";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDzGzhVvNxftVUbxCLudMG6apP5auR56ls",
+  authDomain: "todolist-f0d54.firebaseapp.com",
+  projectId: "todolist-f0d54",
+  storageBucket: "todolist-f0d54.appspot.com",
+  messagingSenderId: "800631363262",
+  appId: "1:800631363262:web:448c743ec556cce326b0ef",
+  measurementId: "G-7544P1V0ZJ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 let todoItemId = 0;
 
 const App = () => {
