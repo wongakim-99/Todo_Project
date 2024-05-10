5fb885073127e3b0502eb253fedf4be702f2aa98
wongakim-99 <202021000@sangmyung.kr>
2024-05-10 18:22:52 +0900

    #20 - 데이터 플로우 리펙터 하기
    이전까지의 App.js 코드를 보면 우리가 setState를 통해서 상태관리를 해줬지만, 코드가 매우 더럽고 복잡함.
    그래서 조금 더 깔끔한 상태를 위해 firestore가 업데이트 될 때마다 다시 firestore에서 다 읽어와서 front state를 업데이트 하는 방식으로 진행함

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -39,7 +39,18 @@ const db = getFirestore(app);
 const App = () => {
   const [todoItemList, setTodoItemList] = useState([]);
 
-  useEffect(() => {
+  /***************************************************************************************/
+  /**firestore를 업데이트 하는 이벤트가 3가지
+   * 1. 새로운 아이템이 추가되었을 때
+   * 2. isFinished를 할때
+   * 3. 데이터를 지울 때
+   * 이 이벤트를 진행할때마다 firestore에 이벤트를 보내고 우리가 front state를 바꿔준다.
+   * 그러나 위와 같이 관리할 경우 굉장히 관리하기가 힘들어짐;;(나중에 state가 잘못되었을 때 헷갈릴수 있음)
+   * 그래서 dataflow를 좀 더 수월하게 관리하기 위해 firestore가 업데이트 될 때마다 다시 firestore에서 다 읽어와서
+   * 우리 state를 업데이트 하는 방식으로 진행*/
+  /***************************************************************************************/
+
+  const syncTodoItemListStateWithFirestore = () => {
     getDocs(collection(db, "todoItem")).then((QuerySnapshot) => {
       const firestoreTodoItemList = [];
       QuerySnapshot.forEach((doc) => {
@@ -51,13 +62,17 @@ const App = () => {
       });
       setTodoItemList(firestoreTodoItemList);
     });
+  };
+
+  useEffect(() => {
+    syncTodoItemListStateWithFirestore();
   }, []);
 
   const onSubmit = async (newTodoItem) => {
     // 함수 내에서 await를 하기 위해서는 async를 해야됨
     /**todoItem이 추가될 때마다 데이터베이스에 작성
      * 기존에는 front base에만 업데이트를 했었지만 이제 firebase의 database에 아래의 내용을 작성*/
-    const docRef = await addDoc(collection(db, "todoItem"), {
+    await addDoc(collection(db, "todoItem"), {
       //await : firebase에 저장을 하면 오래 걸리는 시간을 기다림
       //addDoc : firebase에 쓰라는 의미
       /**뭘 쓰라는 걸까? -> 인자로 전달된 db와todoItem을 보면된다.
@@ -65,16 +80,7 @@ const App = () => {
       todoItemContent: newTodoItem,
       isFinished: false,
     });
-    /***************************************************************************************/
-    setTodoItemList([
-      //위에서 firebase에 쓰고나서 frontend state에 아래와 같이 업데이트
-      ...todoItemList,
-      {
-        id: docRef.id,
-        todoItemContent: newTodoItem,
-        isFinished: false,
-      },
-    ]);
+    syncTodoItemListStateWithFirestore();
   };
 
   const onTodoItemClick = async (clickedTodoItem) => {
@@ -84,31 +90,13 @@ const App = () => {
       { isFinished: !clickedTodoItem.isFinished },
       { merge: true }
     );
-
-    setTodoItemList(
-      todoItemList.map((todoItem) => {
-        if (clickedTodoItem.id === todoItem.id) {
-          return {
-            id: clickedTodoItem.id,
-            todoItemContent: clickedTodoItem.todoItemContent,
-            isFinished: !clickedTodoItem.isFinished,
-          };
-        } else {
-          return todoItem;
-        }
-      })
-    );
+    syncTodoItemListStateWithFirestore();
   };
 
   const onRemoveClick = async (removedTodoItem) => {
     const todoItemRef = doc(db, "todoItem", removedTodoItem.id);
     await deleteDoc(todoItemRef);
-
-    setTodoItemList(
-      todoItemList.filter((todoItem) => {
-        return todoItem.id !== removedTodoItem.id;
-      })
-    );
+    syncTodoItemListStateWithFirestore();
   };
 
   return (
